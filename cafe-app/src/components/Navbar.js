862895e075dec9b1b8ef2da9fbabe9e03e4a9936
randychong <mr.randy.chong@gmail.com>
2022-07-28 11:40:05 -0500

    created a products component; created stylesheets folder

--- cafe-app/src/components/Navbar.js
+++ cafe-app/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
+import Products from './Products';
 import Cart from './Cart';
 
 export default function Navbar() {
@@ -10,11 +11,15 @@ export default function Navbar() {
             <Link to="/home">
                 <p>Link to home</p>
             </Link>
+            <Link to="/products">
+                <p>Link to products</p>
+            </Link>
             <Link to="/cart">
                 <p>Link to cart</p>
             </Link>
             <Routes>
                 <Route path="/home" element={<Home />} />
+                <Route path="/products" element={<Products />} />
                 <Route path="/cart" element={<Cart />} />
             </Routes>
         </Router>
