aff682d51106ddbdf87265c79fb3639a656d09b1
randychong <mr.randy.chong@gmail.com>
2022-08-03 12:02:01 -0500

    added a banner image for home page

--- cafe-app/src/components/Home.js
+++ cafe-app/src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Container, Banner } from "../styled-components/Home-styled-components";
+import BannerImage from "../assets/pastry-and-coffee-horizontal.jpg";
 
 export default function Home() {
   return (
     <div>
-        <h1>Home</h1>
+        <Container>
+          <Banner src={BannerImage} />
+        </Container>
     </div>
   )
 }
---
+++ cafe-app/src/styled-components/Home-styled-components.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    margin-bottom: 2rem;
+    width: 100%;
+`
+
+export const Banner = styled.img`
+width: 100%;
+align-self: center;
+height: 40rem;
+object-fit: cover;
+object-position: 50% 50%;
+`
