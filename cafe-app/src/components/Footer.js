9a6768973add09944721ade619f3d640a2a31233
randychong <mr.randy.chong@gmail.com>
2022-08-09 14:13:03 -0500

    imported footer styled-components and social icons

--- cafe-app/src/components/Footer.js
+++ cafe-app/src/components/Footer.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { 
+    Container,
+    SocialIcon,
+    FooterText
+ } from '../styled-components/Footer-styled-components';
+import Instagram from "../assets/instagram.png";
+import Twitter from "../assets/twitter.png";
+import Facebook from "../assets/facebook.png";
 
 export default function Footer() {
   return (
     <div>
-        <h1>Footer</h1>
+        <Container>
+            <Container>
+                <SocialIcon src={Instagram}/>
+                <SocialIcon src={Twitter}/>
+                <SocialIcon src={Facebook}/>
+            </Container>
+
+            <FooterText>
+                ©2021 The Bobarista
+            </FooterText>
+        </Container>
     </div>
   )
 }
--- cafe-app/src/styled-components/Footer-styled-components.js
+++ cafe-app/src/styled-components/Footer-styled-components.js
@@ -1,2 +1,10 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+`
+
+export const SocialIcon = styled.img`
+`
+
+export const FooterText = styled.p`
+`
