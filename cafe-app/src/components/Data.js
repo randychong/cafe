eb585ae1c581becadb45f69006341e9868c3d617
randychong <mr.randy.chong@gmail.com>
2022-08-24 14:27:51 -0500

    completed data file for products

--- cafe-app/src/components/Data.js
+++ cafe-app/src/components/Data.js
@@ -1,3 +1,87 @@
+import affogato from "../assets/affogato.png";
+import americano from "../assets/americano.png";
+import cappucino from "../assets/cappucino.png";
+import espresso from "../assets/espresso.png";
+import honey from "../assets/honey-green-tea.png";
+import lemon from "../assets/lemon-wintermelon-tea.png";
+import matcha from "../assets/matcha-green-tea.png";
+import lychee from "../assets/lychee-oolong-tea.png";
+import thai from "../assets/thai-tea.png";
+import viet from "../assets/viet-coffee.png";
+
 const data = {
-    
-}
+  products: [
+    {
+      id: 1,
+      name: "Espresso",
+      price: 3.0,
+      image: espresso,
+      quantity: 1,
+    },
+    {
+      id: 2,
+      name: "Americano",
+      price: 3.75,
+      image: americano,
+      quantity: 1,
+    },
+    {
+      id: 3,
+      name: "Cappucino",
+      price: 4.0,
+      image: cappucino,
+      quantity: 1,
+    },
+    {
+      id: 4,
+      name: "Vietnamese Coffee",
+      price: 4.75,
+      image: viet,
+      quantity: 1,
+    },
+    {
+      id: 5,
+      name: "Affogato",
+      price: 6.0,
+      image: affogato,
+      quantity: 1,
+    },
+    {
+      id: 6,
+      name: "Matcha Green Tea",
+      price: 4.0,
+      image: matcha,
+      quantity: 1,
+    },
+    {
+      id: 7,
+      name: "Honey Green Tea",
+      price: 4.25,
+      image: honey,
+      quantity: 1,
+    },
+    {
+      id: 8,
+      name: "Lychee Oolong Tea",
+      price: 4.25,
+      image: lychee,
+      quantity: 1,
+    },
+    {
+      id: 9,
+      name: "Lemon Wintermelon Tea",
+      price: 4.5,
+      image: lemon,
+      quantity: 1,
+    },
+    {
+      id: 10,
+      name: "Thai Tea",
+      price: 4.5,
+      image: thai,
+      quantity: 1,
+    },
+  ],
+};
+
+export default data;
